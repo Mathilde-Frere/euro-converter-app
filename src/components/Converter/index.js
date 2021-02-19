74e4df130a3d0d32791daf7413f0e1b31eb2690f
Mathilde-Frere <mathilde.frere1@gmail.com>
2021-02-19 12:29:03 +0100

    Adaptation of the entry point => Converter component

---
+++ src/components/Converter/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Imports
+import './style.scss';
+
+// == Composant
+const Converter = () => (
+  <div>Converter</div>
+);
+
+// == Props Validation
+
+// == Export
+export default Converter;
