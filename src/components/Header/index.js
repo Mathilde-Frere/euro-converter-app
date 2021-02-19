ce237cd8f9e5c2e9810df3614e7a396ea01a37f2
Mathilde-Frere <mathilde.frere1@gmail.com>
2021-02-19 14:19:06 +0100

    Header's basic structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,10 @@ import './style.scss';
 
 // == Composant
 const Header = () => (
-  <header>Header</header>
+  <header className="header">
+    <h1 className="header__title">Converter</h1>
+    <p className="header__base-amount">1 Euro</p>
+  </header>
 );
 
 // == Props Validation
