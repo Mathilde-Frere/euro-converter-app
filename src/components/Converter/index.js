f4b654f8b10587b1cbc3a2d3f012c5792590418c
Mathilde-Frere <mathilde.frere1@gmail.com>
2021-02-19 15:50:33 +0100

    Converter component => Added toggle button and state to vary open value of the currencies view

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -11,16 +11,43 @@ import currenciesData from 'src/data/currenciesData';
 import './style.scss';
 
 // == Composant
-const Converter = () => (
-  <div className="converter">
-    <Header baseAmount={1} />
-    <Currencies currencies={currenciesData} />
-    <Amount
-      value={1.09}
-      currency="United State Dollar"
-    />
-  </div>
-);
+class Converter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Définition du state du composant Converter
+    this.state = {
+      open: false,
+    };
+
+    // Liaison du contexte de la class (this) à celui de la méthode de class "toggle" via le bind
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    console.log('je veux faire varier la valeur de open');
+    this.setState({
+      open: !this.state.open,
+    });
+    console.log('this.state', this.state);
+  }
+
+  render() {
+    return (
+      <div className="converter">
+        <Header baseAmount={1} />
+        <button type="button" onClick={this.toggle}>Toggle</button>
+        {this.state.open && (
+          <Currencies currencies={currenciesData} />
+        )}
+        <Amount
+          value={1.09}
+          currency="United State Dollar"
+        />
+      </div>
+    );
+  }
+}
 
 // == Props Validation
 
